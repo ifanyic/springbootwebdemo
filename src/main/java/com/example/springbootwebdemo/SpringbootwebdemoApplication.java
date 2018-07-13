ce03df207239f10bbf010da2f208731ffbb79fce
ifanyic <fangshyu@163.com>
2018-07-13 16:58:44 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootwebdemo/SpringbootwebdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebdemoApplication.class, args);
+	}
+}
